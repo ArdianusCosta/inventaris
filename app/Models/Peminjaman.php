127a74caf23df8b4bf3b2b14ce2c594458920020
debbyzilpa <zilpadebby@gmail.com>
2024-11-25 21:13:57 +0700

    pemperbaiki code yang masih erro

--- app/Models/Peminjaman.php
+++ app/Models/Peminjaman.php
@@ -9,24 +9,21 @@ class Peminjaman extends Model
 {
     use HasFactory;
 
-    protected $table = 'peminjaman'; // Nama tabel
+    protected $table = 'peminjaman';
 
-    // Kolom yang boleh diisi secara mass assignment
     protected $fillable = [
         'id_alat',
         'id_peminjaman',
         'jumlah',
         'tgl_peminjaman',
-        'kondisi_pinjam'
+        'kondisi_pinjam',
     ];
 
-    // Relasi dengan model Inventaris
-    public function inventaris()
+    public function alat()
     {
         return $this->belongsTo(Inventaris::class, 'id_alat');
     }
 
-    // Relasi dengan model User (Peminjam)
     public function user()
     {
         return $this->belongsTo(User::class, 'id_peminjaman');
