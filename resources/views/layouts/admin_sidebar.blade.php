d993b04a9038e8e2b9f92d4cb924c21b40e05206
y_satrya <ssatrya717@gmail.com>
2024-11-25 11:52:19 +0700

    project akhir semester

--- resources/views/layouts/admin_sidebar.blade.php
+++ resources/views/layouts/admin_sidebar.blade.php
@@ -1,4 +1,4 @@
-<a href="index3.html" class="brand-link">
+<a href="#" class="brand-link">
     <img src="{{ asset('admin/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
     <span class="brand-text font-weight-light">Inventaris RPL</span>
 </a>
