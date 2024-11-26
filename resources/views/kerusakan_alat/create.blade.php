6a599a210d547266b65eb78e91b8460d813b6918
debbyzilpa <zilpadebby@gmail.com>
2024-11-26 11:37:10 +0700

    tabel kerusakan alat

--- resources/views/kerusakan_alat/create.blade.php
+++ resources/views/kerusakan_alat/create.blade.php
@@ -7,23 +7,23 @@
         @csrf
         <div>
             <label>ID Alat:</label>
-            <input type="text" name="id_alat" required>
+            <input type="text" name="id_alat" class="form-control" required>
         </div>
         <div>
             <label>Spesifikasi:</label>
-            <textarea name="spesifikasi"></textarea>
+            <input type="text" name="Spesifikasi" class="form-control" required>
         </div>
         <div>
             <label>Kerusakan:</label>
-            <textarea name="kerusakan" required></textarea>
+            <input type="text" name="Kerusakan" class="form-control" required>
         </div>
         <div>
             <label>Tanggal Kerusakan:</label>
-            <input type="date" name="tgl_kerusakan" required>
+            <input type="text" name="Tanggal Kerusakan" class="form-control" required>
         </div>
         <div>
             <label>Keterangan:</label>
-            <textarea name="keterangan"></textarea>
+            <input type="text" name="Keterangan" class="form-control" required>
         </div>
         <button type="submit">Simpan</button>
     </form>
